33f30f8f2824ba39323c914d75a91d8309f9e41d
Andrii Kryvyi <87859325+Ronelos@users.noreply.github.com>
2021-12-23 21:01:22 +0200

    hw8-1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const options = {
+    id: 59777392,
+    width: 640,
+    loop: true
+};
+const player = new Player('made-in-ny', options);
+player.setVolume(0);
+
+const onTime = function(data) {
+   console.log(data.seconds);
+   localStorage.setItem("videoplayer-current-time",data.seconds)
+};
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
+    
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
+
+player.on('timeupdate', throttle(onTime, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,56 @@
+import throttle from "lodash.throttle";
 
+const MESSAGE_KEY = 'feedback-form-state';
+const data = {}
+
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('.feedback-form textarea');
+const input = document.querySelector('.feedback-form input');
+
+onMessageOutput();
+onSaved();
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onTextInput, 500));
+form.addEventListener('input', throttle(onConsoleOutput, 500));
+
+function onSaved () {
+    data.email = input.value;
+    data.message = textarea.value;
+}
+
+function onConsoleOutput(event){
+    data[event.target.name] = event.target.value;
+}
+
+function onFormSubmit (event) {
+    event.preventDefault();
+
+    if(!(event.currentTarget.elements.email.value === '' || event.currentTarget.elements.message.value === ''))
+    {
+        event.currentTarget.reset();
+        localStorage.removeItem(MESSAGE_KEY);
+        if(Object.keys(data).length === 0 && Object.values(data).length === 0) return;
+        console.log(data);
+        
+    } else{
+    alert('Все поля должны быть заполнены!');
+    return;}
+}
+
+
+function onTextInput (event) {
+
+
+    if(Object.keys(data).length === 0) return;
+    const dataStr = JSON.stringify(data);
+    localStorage.setItem(MESSAGE_KEY, dataStr);
+}
+
+
+function onMessageOutput () {
+    const savedData = localStorage.getItem(MESSAGE_KEY);
+    if(!savedData) return;
+    textarea.value =  JSON.parse(savedData)["message"];
+    input.value =  JSON.parse(savedData)["email"];
+}
